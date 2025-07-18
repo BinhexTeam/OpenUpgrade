26b245f2497398d695b8d6d58cbc676cb2c5463c
Antonio David Ruban Espinal <antoniodavid8@gmail.com>
2025-07-18 18:21:33 -0400

    [OU-ADD] stock: precompute fields

--- openupgrade_scripts/scripts/sale/17.0.1.2/pre-migration.py
+++ openupgrade_scripts/scripts/sale/17.0.1.2/pre-migration.py
@@ -4,6 +4,26 @@
 from openupgradelib import openupgrade
 
 
+def _precreate_fields_computation(env):
+    if not openupgrade.column_exists(
+        env.cr, "sale_order", "prepayment_percent"
+    ):
+        openupgrade.add_fields(
+            env,
+            [
+                (
+                    "prepayment_percent",
+                    "sale.order",
+                    "sale_order",
+                    "float",
+                    False,
+                    "sale",
+                )
+            ],
+        )
+
+
 @openupgrade.migrate()
 def migrate(env, version):
     openupgrade.copy_columns(env.cr, {"sale_order": [("state", None, None)]})
+    _precreate_fields_computation(env)
